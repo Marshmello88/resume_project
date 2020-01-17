fd4ca846a0b551b0751226bfeac359882c4df03a
Ubuntu <ubuntu@ip-172-31-84-45.ec2.internal>
2020-01-17 20:18:04 +0000

    github styles added

---
+++ assets/js/github-information.js
@@ -0,0 +1,88 @@
+/* make sure that the javaScript files are in place, and that there's HTML 
+that's going to call a JavaScript function.*/
+
+/* And if text has been inputted into the field, then we're going to set our HTML to display a loader.*/
+
+/* when we've got a response from the GitHub API, then run a function to display it in the gh-user-data div.*/
+ 
+/* And the if statement says if !username. So if the username field is empty, there's no value, then we're going to return a little piece of HTML 
+that says "Please enter a GitHub username". */
+
+/*And we're going to say that if the errorResponse.status === 404 (that's a not found error), then what we're going to do is select 
+our gh-user-data div and set its HTML to an error message that says the user wasn't found.
+So in our case, we'll put an h2 heading.*/
+/*And remember at this point that user is the object that's been returned from the GitHub API.
+This object has many methods, such as the user's name, login name, and links to their profile.*/
+
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+}
